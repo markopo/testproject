e61938e6b768a7a726537fd971a0f38a8fc02c87
markopo <marko_poikkimaki@hotmail.com>
2024-07-26 14:20:22 +0200

    spanish

--- main.go
+++ main.go
@@ -4,16 +4,29 @@ import "fmt"
 
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+const spanishHelloPrefix = "Hola, "
+
+func Hello(name string, lang string) string {
 
 	if name == "" {
 		name = "World"
 	}
 
-	return englishHelloPrefix + name
+	prefix := englishHelloPrefix
+
+	switch lang {
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	case "English":
+		prefix = englishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Marko"))
+	fmt.Println(Hello("Marko", "English"))
 
 }
--- main_test.go
+++ main_test.go
@@ -5,14 +5,20 @@ import "testing"
 func TestHello(t *testing.T) {
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Johnny")
+		got := Hello("Johnny", "English")
 		want := "Hello, Johnny"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 }
