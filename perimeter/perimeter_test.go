e99d56b782e8521b6f8c19fff197d93193f2a5f3
markopo <marko_poikkimaki@hotmail.com>
2024-10-15 19:08:38 +0200

    perimeter

---
+++ perimeter/perimeter_test.go
@@ -0,0 +1,33 @@
+package perimeter
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f, want %.2f", got, want)
+	}
+}
+
+func TestRectangleArea(t *testing.T) {
+	rectangle := Rectangle{12.0, 6.0}
+	got := Area(rectangle)
+	want := 72.0
+
+	if got != want {
+		t.Errorf("got %.2f, want %.2f", got, want)
+	}
+}
+
+func TestCircleArea(t *testing.T) {
+	circle := Circle{10.0}
+	got := circle.Area()
+	want := 314.1592653589793
+
+	if got != want {
+		t.Errorf("got %.2f, want %.2f", got, want)
+	}
+}
